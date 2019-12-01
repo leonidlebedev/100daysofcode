7bfc50af10d541f23fc5e2665850529ac65cafe0
Leonid Lebedev <lix.lebedev@yandex.ru>
2019-12-01 12:21:31 +0300

    DAY6: add documentation

---
+++ scripts/replace_characters/README.md
@@ -0,0 +1,16 @@
+# Replace characters
+
+## Usage
+
+```bash
+python3 main.py --from=. --to=, --input-file=example.txt
+```
+
+### Options
+```
+-f, --from          symbol or string
+-t, --to            symbol or string
+-i, --input-file    path to the file
+-h, --help          show help
+```
+
--- scripts/replace_characters/main.py
+++ scripts/replace_characters/main.py
@@ -1,26 +1,45 @@
 import sys, getopt
 
-opts, args = getopt.getopt(sys.argv[1:], 'h:', [
-  'from=', 'to=', 'input-file='
-])
+try:
+  opts, args = getopt.getopt(sys.argv[1:], 'hHi:f:t:', [
+    'from=', 'to=', 'input-file=', 'help',
+  ])
+except getopt.error as msg:
+  print(msg)
+  print("""\
+  Help: python3 %s -h
+  """%sys.argv[0])
+  sys.exit(2)
 
-from_character = '.'
-to_character = ','
-plaintext = None
-filename = None
+from_character = ''
+to_character = ''
+plaintext = ''
+filename = ''
 
 for o, v in opts:
-  if o == '--from':
+  if o in ('-h', '-H', '--help'):
+    print((
+      '\n'
+      'Usage: python3 %s [options]'
+      '\n\n'
+      'Options:\n\n'
+      '-f, --from          symbol or string\n'
+      '-t, --to            symbol or string\n'
+      '-i, --input-file    path to the file\n'
+      '-h, --help          show help'
+    )%sys.argv[0])
+    sys.exit(0)
+  if o in ('-f', '--from'):
     if len(v) == 0:
       print('Empty from option, see -h')
       sys.exit(1)
     from_character = v
-  elif o == '--to':
+  elif o in ('-t', '--to'):
     if len(v) == 0:
       print('Empty to option, see -h')
       sys.exit(1)
     to_character = v
-  elif o == '--input-file':
+  elif o in ('-i', '--input-file'):
     try:
       with open(v, 'r') as data:
         filename = v
@@ -31,8 +50,12 @@ for o, v in opts:
 
 plaintext = plaintext.replace(from_character, to_character)
 
-with open(filename, 'w') as file:
-  file.write(plaintext)
+try:
+  with open(filename, 'w') as file:
+    file.write(plaintext)
+except IOError as e:
+  print(e)
+  sys.exit(1)
 
 print('Excellento')
 sys.exit(0)
